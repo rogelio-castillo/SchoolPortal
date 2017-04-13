981e175a951c94f45008ef9760d567065dc5b888
Shahbaz Surani <suranisha@yahoo.com>
2017-04-13 15:19:36 -0400

    added folders

---
+++ _includes/user.php
@@ -0,0 +1,32 @@
+<?
+class Teacher{
+	
+	public $id;
+	public $firstName;
+	public $lastName;
+	
+	function __constructor($id, $firstName, $lastName){
+		$this->id=$id;
+		$this->firstName=$firstName;
+		$this->lastName=$lastName;
+		$this->create();
+	}
+	
+	public function get($id){
+		
+	}
+	
+	private function create(){
+		
+	}
+	
+	public function delete(){
+		
+	}
+	
+	public function update(){
+		
+	}
+	
+}
+?>
