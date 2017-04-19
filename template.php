d4344681f9878b6cec7a80375468374aa7e0003e
Shahbaz Surani <suranisha@yahoo.com>
2017-04-18 21:34:54 -0400

    created template files. seperated header, footer, table, and form in seperate file.

--- template.php
+++ template.php
@@ -1,164 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <meta name="description" content="">
-    <meta name="author" content="">
 
-    <title>Starter Template for Bootstrap</title>
-
-    <!-- Bootstrap core CSS -->
-    <link href="_css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="_css/ie10-viewport-bug-workaround.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="_css/main.css" rel="stylesheet">
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="_js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="_js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  
-  	
-  
+  	<?php require_once("_includes/init.php"); ?>
+  	<?php require_once("_files/header.php"); ?>
+    
+  	<title>Starter Template for Bootstrap</title>
   </head>
 
   <body>
 
-    <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
-        </div>
-        <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
+    <?php require_once("_files/menu.php"); ?>
 
     <div class="container myContent">
-    	<? //form code start here ?>
-        <form class ="form-horizontal">
-		  		 
-		  <div class="form-group">
-			<label for="username" class ="control-label col-md-2">Username</label>
-			<div class ="col-sm-10">
-			<input type="text" class="form-control" id="username" placeholder="User Name">
-			</div>
-		  </div>	
-		  
-		  <div class="form-group">
-			<label for="firstname" class ="control-label col-sm-2">First Name</label>
-			<div class ="col-sm-10">
-			<input type="text" class="form-control" id="firstname" placeholder="First Name">
-			</div>
-		  </div>
-		  
-		  <div class="form-group">
-			<label for="lastname" class ="control-label col-sm-2">Last Name</label>
-			<div class ="col-sm-10">
-			<input type="text" class="form-control" id="lastname" placeholder="Last Name">
-			</div>
-		  </div>
-		  
-		  <div class = "form-group">
-		  <div class ="col-sm-10 col-sm-offset-2">
-		  <button type="submit" class="btn btn-default">Submit</button>
-		  </div>
-		  </div>
-        </form>
+    	
+        <?php require_once("_files/templateFiles/form.html"); ?>
         
-		<? //form 2 ?>
-        <form>
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-          </div>
-          <div class="form-group">
-            <label for="exampleInputFile">File input</label>
-            <input type="file" id="exampleInputFile">
-            <p class="help-block">Example block-level help text here.</p>
-          </div>
-          <div class="checkbox">
-            <label>
-              <input type="checkbox"> Check me out
-            </label>
-          </div>
-          <button type="submit" class="btn btn-default">Submit</button>
-        </form>
+        <?php require_once("_files/templateFiles/table.html"); ?>
         
-        
-        <? //form code ends here ?>
-        
-        
-        <? //table code start here ?>
-        <table class="table table-bordered">
-          <thead>
-            <tr>
-              <th>#</th>
-              
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
-            </tr>
-          </thead>
-           <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry the Bird</td>
-              <td>ton</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
-         </table>
-        <? //table code ends here ?>
-
     </div><!-- /.container -->
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="_js/bootstrap.min.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="_js/ie10-viewport-bug-workaround.js"></script>
+    
+	<?php require_once("_files/footer.php"); ?>
+    
   </body>
 </html>
