305f9a0bd58d9dd87a21971c88f3a36ed199f00e
Avani Shah <avi_kalol@yahoo.com>
2017-04-20 17:25:03 -0400

    todolist.php

---
+++ to-do-list.php
@@ -0,0 +1,78 @@
+
+  	<?php require_once("_includes/init.php"); ?>
+  	<?php require_once("_files/header.php"); ?>
+    
+  	<title>Starter Template for Bootstrap</title>
+  </head>
+
+  <body>
+
+    <?php require_once("_files/menu.php"); ?>
+
+    <div class="container myContent row">
+    	
+       <h1>Teacher View</h1>
+		<form>
+		  <div class="form-group">
+			<label for="teacher">Teacher</label>
+			<input name = "teacher" type="text" class="form-control" id="teacher" placeholder="Enter name">
+		
+		  </div>
+		  <div class="form-group">
+			<label for="task">Task</label>
+			<input name = "task" type="text" class="form-control" id="task" placeholder="Enter task(s) here">
+		  </div>
+		
+		  <button name = "add" type="submit" class="btn btn-primary" style="margin: 10px auto;">Add to List</button>
+		</form>
+	
+	<h1>Parent View</h1>
+	
+		<table class="table">
+			  <thead class="thead-default">
+				<tr>
+				  <th>No.</th>
+				  <th>To-do-list</th>
+				  <th>Due Date</th>
+				</tr>
+			  </thead>
+			  <tbody>
+				<tr>
+				  <th scope="row">1</th>
+				  <td></td>
+				  <td></td>
+				  
+				</tr>
+				<tr>
+				  <th scope="row">2</th>
+				  <td></td>
+				  <td></td>
+				  
+				</tr>
+				
+			  </tbody>
+			</table>
+			
+			
+			<div class='col-sm-6'>
+				<div class="form-group">
+					<div class='input-group date' id='datetimepicker1'>
+						<input type='text' class="form-control" />
+						<span class="input-group-addon">
+							<span class="glyphicon glyphicon-calendar"></span>
+						</span>
+					</div>
+				</div>
+			</div>
+        
+    </div><!-- /.container -->
+    
+	<?php require_once("_files/footer.php"); ?>
+			<script type="text/javascript">
+				$(function () {
+					$('#datetimepicker1').datetimepicker();
+				});
+			</script>
+			
+  </body>
+</html>
