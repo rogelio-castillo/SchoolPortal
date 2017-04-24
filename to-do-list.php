c08bf780e3e49291bef09a11d866d314bb8777b1
Avani Shah <avi_kalol@yahoo.com>
2017-04-24 16:43:42 -0400

    to-do-list page

--- to-do-list.php
+++ to-do-list.php
@@ -5,93 +5,112 @@
 		redirect_to("index.php");
 	}
 	$user = User::userinfo();
-	if (!isset($_GET["classid"]) || !_Class::isvalid($_GET["classid"])){
+	if (!isset($_GET["classid"]) || !_class::isvalid($_GET["classid"])){
 		redirect_to("userinfo.php");
-		
 	}
-	
-	
-	//you will get all the to-do list from db
-	//check if add todolist form is submited then validate and add everything to db
-	//display all messages
+	$classid = $_GET["classid"]; 
 	
 	
 	
-  	require_once("_files/header.php"); ?>
+	//check if add todolist form is submited then validate and add everything to db	
+	
+  	require_once("_files/header.php"); 
+	?>
     
-  	<title>To-do list</title>
-  </head>
+	<title>To-do list</title>
+	
+		<!--  jQuery -->
+		<script type="text/javascript" src="https://code.jquery.com/jquery-1.11.3.min.js"></script>
+
+		<!-- Isolated Version of Bootstrap, not needed if your site already uses Bootstrap -->
+		<link rel="stylesheet" href="https://formden.com/static/cdn/bootstrap-iso.css" />
+
+		<!-- Bootstrap Date-Picker Plugin -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
+	
+    </head>
 
   <body>
   
-    <?php require_once("_files/menu.php"); ?>
-
-	
-	
-    <div class="container myContent row">
-	
-       <h1>Teacher View</h1>
-		<form>
-		  <div class="form-group">
-			<label for="teacher">Teacher</label>
-			<input name = "teacher" type="text" class="form-control" id="teacher" placeholder="Enter name">
+    <?php require_once("_files/menu.php"); 
 		
-		  </div>
+		//Selects all table contents by class id
+		$sql="SELECT * FROM todolist 
+			  WHERE classid='{$classid}' ORDER BY date ASC";
+			  $result=$db->query($sql);
+			//$result=mysqli_query($sql);
+
+	?>
+	
+    <div class="container">
+	
+	<?php
+		//if($user->type==1) : 
+		if($user->type==1){ ?>
+		<h1>Teacher View</h1>
+		
+		<form action="task.php?classid=<?php echo $classid; ?>" method= "POST">
+
 		  <div class="form-group">
 			<label for="task">Task</label>
 			<input name = "task" type="text" class="form-control" id="task" placeholder="Enter task(s) here">
 		  </div>
-		
-		  <button name = "add" type="submit" class="btn btn-primary" style="margin: 10px auto;">Add to List</button>
+		  
+		  <div class="form-group bootstrap-iso"> <!-- Date input -->
+			<label class="control-label" for="date">Due Date</label>
+			<input class="form-control" id="date" name="date" placeholder="YYYY/MM/DD" type="text"/>
+		  </div>
+		  
+		  <button name = "add" type="submit" class="btn btn-primary" style="margin: 10px auto;">Add to List</button> 
 		</form>
+		<?php } ?>
 	
-	<h1>Parent View</h1>
+		<!--<h1>Common View</h1>-->
 	
 		<table class="table">
 			  <thead class="thead-default">
 				<tr>
 				  <th>No.</th>
+				  <th>Class ID</th>
 				  <th>To-do-list</th>
 				  <th>Due Date</th>
 				</tr>
 			  </thead>
-			  <tbody>
-				<tr>
-				  <th scope="row">1</th>
-				  <td></td>
-				  <td></td>
-				  
-				</tr>
-				<tr>
-				  <th scope="row">2</th>
-				  <td></td>
-				  <td></td>
-				  
-				</tr>
 				
-			  </tbody>
-			</table>
-			
-			
-			<div class='col-sm-6'>
-				<div class="form-group">
-					<div class='input-group date' id='datetimepicker1'>
-						<input type='text' class="form-control" />
-						<span class="input-group-addon">
-							<span class="glyphicon glyphicon-calendar"></span>
-						</span>
-					</div>
-				</div>
-			</div>
-        
+			  <tbody>
+				<?php
+				//display task list	
+				//Loops through table rows and prints the values
+				while($rows=$db->fetch_array($result)){
+					?>		
+						<td><?php echo $rows['id'];?></td>
+						<td><?php echo $rows['classid'];?></td>
+						<td><?php echo $rows['task'];?></td>
+						<td><?php echo $rows['date'];?></td>
+					</tr>
+				<?php
+					}
+				?>
+				</tbody>
+			</table>     
     </div><!-- /.container -->
     
 	<?php require_once("_files/footer.php"); ?>
-			<script type="text/javascript">
-				$(function () {
-					$('#datetimepicker1').datetimepicker();
-				});
-			</script>
 			
+	<script>
+    $(document).ready(function(){
+      var date_input=$('input[name="date"]'); //our date input has the name "date"
+      var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
+      var options={
+		format: 'yyyy/mm/dd',
+        //format: 'mm/dd/yyyy',
+        container: container,
+        todayHighlight: true,
+        autoclose: true,
+      };
+      date_input.datepicker(options);
+    })
+	</script>		
   </body>
 </html>
