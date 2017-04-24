8ef30bca4319e7b81acd0cccb12e34000d20df54
Avani Shah <avi_kalol@yahoo.com>
2017-04-24 17:25:59 -0400

    todolist.sql

--- to-do-list.php
+++ to-do-list.php
@@ -103,8 +103,8 @@
       var date_input=$('input[name="date"]'); //our date input has the name "date"
       var container=$('.bootstrap-iso form').length>0 ? $('.bootstrap-iso form').parent() : "body";
       var options={
-		format: 'yyyy/mm/dd',
-        //format: 'mm/dd/yyyy',
+		//format: 'yyyy/mm/dd',
+        format: 'mm/dd/yyyy',
         container: container,
         todayHighlight: true,
         autoclose: true,
